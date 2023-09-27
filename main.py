4b8b274aee74539386c55ac57388bd8cac6c8c8b
Robert Horvat <robert.horvath93@gmail.com>
2023-09-27 22:21:41 +0300

    Delta_Frame

--- main.py
+++ main.py
@@ -6,12 +6,19 @@ import time
 video = cv2.VideoCapture(0)
 time.sleep(1)
 
+first_frame = None
 while True:
     check1, frame = video.read()
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     # converting to grayscale pixels
     gray_frame_gau = cv2.GaussianBlur(gray_frame, (21, 21), 0)
-    cv2.imshow("My video", gray_frame_gau)
+
+    if first_frame is None:
+        first_frame = gray_frame_gau
+
+    delta_frame = cv2.absdiff(first_frame, gray_frame_gau)
+    cv2.imshow("My video", delta_frame)
+    print(delta_frame)
 
     key = cv2.waitKey(1)
 
